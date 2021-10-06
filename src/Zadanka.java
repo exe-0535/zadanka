eca80a81ea1eaf65db28bbbdb9dda04e3d0217d1
exe <jjaypghd@gmail.com>
2021-10-06 22:20:50 +0200

    3 zadanka

--- src/Zadanka.java
+++ src/Zadanka.java
@@ -13,8 +13,7 @@ public class Zadanka {
         int n = -1;
 
         while(n != 0) {
-            System.out.println("Wprowadź liczbę: ");
-            n = skaner.nextInt();
+            System.out.println("Wprowadź liczbę: "); n = skaner.nextInt();
             System.out.println("Wprowadzono " + n + ".");
         }
 
@@ -31,6 +30,25 @@ public class Zadanka {
         System.out.println("Silnia liczby " + num + " to " + s);
 
         // Zad 3
+
+        System.out.println("Podaj login: "); String login = skaner.next();
+        System.out.println("Podaj hasło: "); String password = skaner.next();
+
+        System.out.println("Zaloguj się: ");
+
+        for(int i = 1; i < 3; i++) {
+
+            System.out.println("Podaj login: "); String loginCheck = skaner.next();
+            System.out.println("Podaj hasło: "); String passwordCheck = skaner.next();
+
+            if(loginCheck.equals(login) && passwordCheck.equals(password)) {
+                System.out.println("Wprowadzono poprawne dane."); break;
+            } else {
+                System.out.println("Wprowadzono błędny login lub hasło. Spróbuj ponownie. Pozostało prób: " + (3 - i));
+            }
+        }
+
+        // Zad 4
 
 
     }
