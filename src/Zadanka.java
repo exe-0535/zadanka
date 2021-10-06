b0cb912d4853e7f2b0d5fd9af9b3cb18b271fe7a
exe <jjaypghd@gmail.com>
2021-10-06 22:12:01 +0200

    2 zadanka

--- src/Zadanka.java
+++ src/Zadanka.java
@@ -2,10 +2,35 @@ import java.util.Scanner;
 
 public class Zadanka {
 
-    public Scanner skaner = new Scanner(System.in);
+    private static final Scanner skaner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
+        // PETLE
+
+        // Zad 1
+
+        int n = -1;
+
+        while(n != 0) {
+            System.out.println("Wprowadź liczbę: ");
+            n = skaner.nextInt();
+            System.out.println("Wprowadzono " + n + ".");
+        }
+
+        // Zad 2
+
+        System.out.println("Wprowadź liczbę do obliczenia silni: ");
+
+        int s = 1;
+        int num = skaner.nextInt();
+        for(int i=1; i <= num; i++){
+            s = s * i;
+        }
+
+        System.out.println("Silnia liczby " + num + " to " + s);
+
+        // Zad 3
 
 
     }
